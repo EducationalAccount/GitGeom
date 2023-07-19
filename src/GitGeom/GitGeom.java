83fb47ea88fc42ac9376d86b322030e7a1e4ad98
DmytroBond <bond_dv@ukr.net>
2023-07-19 16:10:02 +0300

    com 002 19.07.23

--- src/GitGeom/GitGeom.java
+++ src/GitGeom/GitGeom.java
@@ -5,6 +5,8 @@ public class GitGeom {
 	public static void main(String[] args) {
 		Point2D p = new Point2D(0.0, 0.0);
 		System.out.println("p = " + p);
+		System.out.println("p.X = " + p.getX());
+		System.out.println("p.Y = " + p.getY());
 	}
 
 }
